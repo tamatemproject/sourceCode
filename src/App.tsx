4da4673bb0a89d40b7c4b07a46d9c1d2c0910572
alaa.hamed <alaa.hamed@cegedim.com>
2021-11-10 12:42:13 +0100

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        this is test branch
+        this is new branch
         </p>
         <a
           className="App-link"

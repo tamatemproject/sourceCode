5e697848901559de974d866e2abfc13a90519506
Melhawy <mariam.elhawy@cegedim.com>
2021-11-24 00:54:49 +0200

    create initial custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { Button } from '@material-ui/core';;
+import { Button } from '@material-ui/core';
+import customTheme from './assets/themes/Default/Theme';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <Button variant="contained" color="primary">primary</Button>
         <Button variant="contained" color="secondary">secondary</Button>
-        <Button variant="contained" >tertiary</Button>
+        <Button variant="contained" >Default</Button>
       </header>
       </div>
   );
--- src/assets/themes/Default/Theme.js
+++ src/assets/themes/Default/Theme.js
@@ -9,12 +9,24 @@ export const SecondaryColors = {
 export const TertiaryColors = {
   main: "#FA9441",
 };
+export const TextColors = {
+  main: "#1E3A56",
+};
+export const GreyColors = {
+  main: "#B0CAD1",
+};
 
 const customTheme = createTheme({
   palette: {
     primary: { ...PrimaryColors, contrastText: "#fff" },
     secondary: { ...SecondaryColors, contrastText: "#fff" },
     tertiary: { ...TertiaryColors, contrastText: "#fff" },
+    text: { ...TextColors, contrastText: "#fff" },
+    grey: { ...GreyColors, contrastText: "#fff" },
+  },
+  spacing: 10,
+  shape: {
+    borderRadius: 23.5,
   },
 });
 
